6fa508080905cbb68871e280ab2a64eb8741394c
skorpion <jmosinski@o2.pl>
2024-07-09 21:34:04 +0200

    changed structure in Home

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,26 +1,13 @@
-import React, { useContext } from 'react'
+import React from 'react'
 
-import styles from './Home.module.css'
-import HomeContext from './Context/HomeContext'
+import HomeContent from './HomeContent'
+import { HomeProvider } from './Context/HomeContext'
 
 const Home = () => {
-	const { isActivePL, isActiveEN, addedText } = useContext(HomeContext)
-
-	console.log(isActivePL)
-	console.log(isActiveEN)
-
 	return (
-		<section className={styles.wrapper}>
-			<div className={styles.box_btns}>
-				<button className={styles.btn_pl} onClick={addedText}>
-					Polski
-				</button>
-				<button className={styles.btn_en}>English</button>
-			</div>
-			<div className={styles.box_content}>
-				<p className={styles.text}></p>
-			</div>
-		</section>
+		<HomeProvider>
+			<HomeContent />
+		</HomeProvider>
 	)
 }
 
---
+++ src/Components/Home/HomeContent.tsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+
+import styles from './Home.module.css'
+import HomeContext from './Context/HomeContext'
+
+const HomeContent = () => {
+	const { textPL, textEN, isActivePL, isActiveEN, addedText } = useContext(HomeContext)
+
+	return (
+		<section className={styles.wrapper}>
+			<div className={styles.box_btns}>
+				<button className={styles.btn_pl} onClick={addedText}>
+					Polski
+				</button>
+				<button className={styles.btn_en}>English</button>
+			</div>
+			<div className={styles.box_content}>
+				<p className={styles.text}></p>
+			</div>
+		</section>
+	)
+}
+
+export default HomeContent
