0c96bfdbe510b6ef72f1b67b93970bda4ccb512c
skorpion <jmosinski@o2.pl>
2024-07-11 18:57:17 +0200

    success with pages test

--- src/Components/WeatherApp/NavWorld.tsx
+++ src/Components/WeatherApp/NavWorld.tsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const NavWorld = () => {
 	return (
 		<div>
-			<Link to='./Poland'>
+			<Link to='/WeatherApp/Poland'>
 				<h4>POLAND</h4>
 			</Link>
-			<Link to='./Europe'>
+			<Link to='/WeatherApp/Europe'>
 				<h4>EUROPA</h4>
 			</Link>
 		</div>
