9eddb2c1a7fe46db8f6ea13f7154fe503a3c64a0
skorpion <jmosinski@o2.pl>
2024-07-12 18:08:57 +0200

    added useLocation

--- src/Components/WeatherApp/NavWorld.tsx
+++ src/Components/WeatherApp/NavWorld.tsx
@@ -1,41 +1,28 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 import styles from './NavWorld.module.css'
 
 const NavWorld = () => {
-	const [activeButton, setActiveButton] = useState<string>('')
-
-	const handleActive = (button: string) => {
-		setActiveButton(button)
-	}
+	const location = useLocation()
 
 	return (
 		<nav className={styles.nav_btns}>
-			<button
-				className={activeButton === 'main' ? styles.btn_active : styles.btn_no_active}
-				onClick={() => handleActive('main')}
+			<Link to='/WeatherApp' className={location.pathname === '/WeatherApp' ? styles.btn_active : styles.btn_no_active}>
+				<p className={styles.into_btn}>Main</p>
+			</Link>
+			<Link
+				to='/WeatherApp/Poland'
+				className={location.pathname === '/WeatherApp/Poland' ? styles.btn_active : styles.btn_no_active}
 			>
-				<Link to='/WeatherApp'>
-					<p className={styles.into_btn}>Main</p>
-				</Link>
-			</button>
-			<button
-				className={activeButton === 'poland' ? styles.btn_active : styles.btn_no_active}
-				onClick={() => handleActive('poland')}
+				<p className={styles.into_btn}>Poland</p>
+			</Link>
+			<Link
+				to='/WeatherApp/Europe'
+				className={location.pathname === '/WeatherApp/Europe' ? styles.btn_active : styles.btn_no_active}
 			>
-				<Link to='/WeatherApp/Poland'>
-					<p className={styles.into_btn}>Poland</p>
-				</Link>
-			</button>
-			<button
-				className={activeButton === 'europe' ? styles.btn_active : styles.btn_no_active}
-				onClick={() => handleActive('europe')}
-			>
-				<Link to='/WeatherApp/Europe'>
-					<p className={styles.into_btn}>Europe</p>
-				</Link>
-			</button>
+				<p className={styles.into_btn}>Europe</p>
+			</Link>
 		</nav>
 	)
 }
