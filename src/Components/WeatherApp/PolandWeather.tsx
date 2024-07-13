a0c6f42899ad56c7701103bae294e1fdcf9c558e
skorpion <jmosinski@o2.pl>
2024-07-13 20:54:20 +0200

    added first city to map

--- src/Components/WeatherApp/PolandWeather.tsx
+++ src/Components/WeatherApp/PolandWeather.tsx
@@ -10,8 +10,16 @@ const PolandWeather = () => {
 	const { refresh, cityPol01, imgPol01, tempPol01, windPol01, pressurePol01 } = useContext(PolandContext)
 
 	const CityPolStetin = (
-		<div>
-			<p>Jest</p>
+		<div className={styles.city_weather}>
+			<div className={styles.top}>
+				<h4 className={styles.name_city}>{cityPol01}</h4>
+				<img className={styles.img_city} src={imgPol01} alt='' />
+			</div>
+			<div className={styles.bottom}>
+				<span className={styles.city_info_weather}>{tempPol01}</span>
+				<span className={styles.city_info_weather}>{windPol01}</span>
+				<span className={styles.city_info_weather}>{pressurePol01}</span>
+			</div>
 		</div>
 	)
 
@@ -23,7 +31,7 @@ const PolandWeather = () => {
 				<div className={styles.box_map}>
 					<img className={styles.img_poland_map} src={PolandMap} alt='poland_map' />
 				</div>
-				<div className={styles.box_cities}>{CityPolStetin}</div>
+				<div className={styles.box_cities_01}>{CityPolStetin}</div>
 			</div>
 		</section>
 	)
