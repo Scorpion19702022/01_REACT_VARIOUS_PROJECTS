2d7e2cd058eea8d7ea51ef5af9d5edc6dc29446f
skorpion <jmosinski@o2.pl>
2024-07-13 18:02:07 +0200

    corrected in styles css

--- src/Components/WeatherApp/PolandWeather.tsx
+++ src/Components/WeatherApp/PolandWeather.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import NavWorld from './NavWorld'
 
+import PolandMap from './assets/polandMAP.png'
+
 import styles from './PolandWeather.module.css'
 
 const PolandWeather = () => {
 	return (
 		<section className={styles.wrapper}>
 			<NavWorld />
-			<div className={styles.box_content}></div>
+			<div className={styles.box_content}>
+				<div className={styles.box_map}>
+					<img className={styles.img_poland_map} src={PolandMap} alt='poland_map' />
+				</div>
+				<div className={styles.box_cities}></div>
+			</div>
 		</section>
 	)
 }
