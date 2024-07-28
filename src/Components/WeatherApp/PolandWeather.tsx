1e73df60c52c7160bf29422d0a2c4b4f30133efc
skorpion <jmosinski@o2.pl>
2024-07-28 19:23:10 +0200

    test

--- src/Components/WeatherApp/PolandWeather.tsx
+++ src/Components/WeatherApp/PolandWeather.tsx
@@ -29,11 +29,15 @@ const PolandWeather = () => {
 		<div className={styles.city_weather}>
 			<div className={styles.top}>
 				<h4 className={styles.name_city}>{cityPol01}</h4>
-				<img className={styles.img_city} src={imgPol01} alt='' />
 			</div>
 			<div className={styles.bottom}>
-				<span className={styles.city_info_weather}>{tempPol01}</span>
-				<span className={styles.city_info_weather}>{windPol01}</span>
+				<div className={styles.box_img}>
+					<img className={styles.img_city} src={imgPol01} alt='weather_icon' />
+				</div>
+				<div className={styles.box_info}>
+					<span className={styles.city_info_weather}>{tempPol01}</span>
+					<span className={styles.city_info_weather}>{windPol01}</span>
+				</div>
 			</div>
 		</div>
 	)
@@ -42,11 +46,15 @@ const PolandWeather = () => {
 		<div className={styles.city_weather}>
 			<div className={styles.top}>
 				<h4 className={styles.name_city}>{cityPol02}</h4>
-				<img className={styles.img_city} src={imgPol02} alt='' />
 			</div>
 			<div className={styles.bottom}>
-				<span className={styles.city_info_weather}>{tempPol02}</span>
-				<span className={styles.city_info_weather}>{windPol02}</span>
+				<div className={styles.box_img}>
+					<img className={styles.img_city} src={imgPol02} alt='weather_icon' />
+				</div>
+				<div className={styles.box_info}>
+					<span className={styles.city_info_weather}>{tempPol02}</span>
+					<span className={styles.city_info_weather}>{windPol02}</span>
+				</div>
 			</div>
 		</div>
 	)
@@ -59,8 +67,8 @@ const PolandWeather = () => {
 				<div className={styles.box_map}>
 					<img className={styles.img_poland_map} src={PolandMap} alt='poland_map' />
 				</div>
-				<div className={styles.box_cities_01}>{CityPol01}</div>
-				<div className={styles.box_cities_02}>{CityPol02}</div>
+				<div className={styles.box_cities}>{CityPol01}</div>
+				<div className={styles.box_cities}>{CityPol02}</div>
 			</div>
 		</section>
 	)
