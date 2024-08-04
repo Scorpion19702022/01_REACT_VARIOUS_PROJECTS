5a3cd430d22c75428f746bdcead6a8e6ede0b679
skorpion <jmosinski@o2.pl>
2024-08-04 19:35:01 +0200

    added next cities for Europe

--- src/Components/WeatherApp/Context/EuropeContext.tsx
+++ src/Components/WeatherApp/Context/EuropeContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 import Un from '../assets/unknown.png'
 import Sun from '../assets/sun.png'
@@ -23,7 +23,13 @@ type EuropeProviderType = {
 
 const InitialState: InitialStateType = {
 	refresh: 0,
-	citiesEurope: [{ city: 'Reykjavík', img: Un, idWeather: 0, temp: 0, wind: 0 }],
+	citiesEurope: [
+		{ city: 'Reykjavík', img: Un, idWeather: 0, temp: 0, wind: 0 },
+		{ city: 'Dublin', img: Un, idWeather: 0, temp: 0, wind: 0 },
+		{ city: 'Londyn', img: Un, idWeather: 0, temp: 0, wind: 0 },
+		{ city: 'Oslo', img: Un, idWeather: 0, temp: 0, wind: 0 },
+		{ city: 'Sztokholm', img: Un, idWeather: 0, temp: 0, wind: 0 },
+	],
 	getWeatherImage: (idWeather: number) => {},
 }
 
@@ -63,6 +69,64 @@ export const EuropeProvider = ({ children }: EuropeProviderType) => {
 		clearTimeout(time)
 		setRefresh(10)
 	}
+
+	const API_LINK_EUROPE = `https://api.openweathermap.org/data/2.5/weather?q=`
+
+	const API_KEY_EUROPE = '&appid=24582e46fb113d7a512e20a446965d23'
+
+	const API_UNITS_EUROPE = '&units=metric'
+
+	useEffect(() => {
+		const cityWeatherEuropeApi = async (city: string, index: number) => {
+			const URL = `${API_LINK_EUROPE}${city}${API_KEY_EUROPE}${API_UNITS_EUROPE}`
+			try {
+				const response = await fetch(URL)
+				if (!response.ok) {
+					throw new Error('error')
+				}
+				const data = await response.json()
+				const codId = Object.assign({}, ...data.weather)
+				const temp = data.main.temp.toFixed(1)
+				const wind = data.wind.speed.toFixed(1)
+
+				setCitiesEurope(prevCities => {
+					const updatedCities = [...prevCities]
+					updatedCities[index] = {
+						...updatedCities[index],
+						idWeather: codId.id,
+						temp: temp,
+						wind: wind,
+						img: getWeatherImage(codId.id),
+					}
+					return updatedCities
+				})
+			} catch (error) {
+				console.log('error')
+				setCitiesEurope(prevCities => {
+					const updatedCities = [...prevCities]
+					updatedCities[index] = {
+						...updatedCities[index],
+						city: 'ERROR',
+						img: Un,
+						temp: 'ERROR',
+						wind: 'ERROR',
+					}
+					return updatedCities
+				})
+			}
+		}
+		citiesEurope.forEach((city, index) => {
+			cityWeatherEuropeApi(city.city, index)
+		})
+
+		const interval = setInterval(() => {
+			citiesEurope.forEach((city, index) => {
+				cityWeatherEuropeApi(city.city, index)
+			})
+		}, 600000)
+
+		return () => clearInterval(interval)
+	}, [])
 
 	return <EuropeContext.Provider value={{ refresh, citiesEurope, getWeatherImage }}>{children}</EuropeContext.Provider>
 }
