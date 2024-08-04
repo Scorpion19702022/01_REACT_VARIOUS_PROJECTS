82cdc7c9059d3f6b14f25a322b50e49638b2053d
skorpion <jmosinski@o2.pl>
2024-08-04 19:14:02 +0200

    test with first city in Europe

--- src/Components/WeatherApp/EuropeWeather.tsx
+++ src/Components/WeatherApp/EuropeWeather.tsx
@@ -8,7 +8,29 @@ import NavWorld from './NavWorld'
 import EuropeContext from './Context/EuropeContext'
 
 const EuropeWeather = () => {
-	const { refresh } = useContext(EuropeContext)
+	const { refresh, citiesEurope } = useContext(EuropeContext)
+
+	const citiesOfEurope = citiesEurope.map((city, index) => (
+		<div key={index} className={styles.box_cities}>
+			<div className={styles.top}>
+				<h4 className={styles.name_city}>{city.city}</h4>
+			</div>
+			<div className={styles.bottom}>
+				<div className={styles.box_img}>
+					<img className={styles.img_city} src={city.img} alt='' />
+				</div>
+				<div className={styles.box_info}>
+					<span className={styles.city_info_weather}>
+						{city.temp} <span className={styles.parametr}> ℃</span>
+					</span>
+					<span className={styles.city_info_weather}>
+						{city.wind} <span className={styles.parametr}> km/h</span>
+					</span>
+				</div>
+			</div>
+		</div>
+	))
+
 	return (
 		<section className={styles.wrapper}>
 			<NavWorld />
@@ -17,6 +39,7 @@ const EuropeWeather = () => {
 				<div className={styles.box_map}>
 					<img className={styles.img_europe_map} src={EuropeMap} alt='' />
 				</div>
+				<div className={styles.city_weather}>{citiesOfEurope}</div>
 			</div>
 		</section>
 	)
