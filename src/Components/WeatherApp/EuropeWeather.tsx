f2527d50d135c14331eb0a3ae2c1a74d039b883d
skorpion <jmosinski@o2.pl>
2024-08-04 18:37:47 +0200

    add map in component EuropeWeather

--- src/Components/WeatherApp/EuropeWeather.tsx
+++ src/Components/WeatherApp/EuropeWeather.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
+import EuropeMap from './assets/EuropeMAP.png'
+
 import styles from './EuropeWeather.module.css'
+
 import NavWorld from './NavWorld'
 
 const EuropeWeather = () => {
 	return (
 		<section className={styles.wrapper}>
 			<NavWorld />
-			<h1>europa</h1>
+			<div className={styles.box_content}>
+				<h4 className={styles.count}>Aktualizacja pogody za 0 min.</h4>
+				<div className={styles.box_map}>
+					<img className={styles.img_europe_map} src={EuropeMap} alt='' />
+				</div>
+			</div>
 		</section>
 	)
 }
