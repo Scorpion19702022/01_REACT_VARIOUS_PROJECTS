b3530c36c3bb19a2df716848e5f25f94145534a4
skorpion <jmosinski@o2.pl>
2024-08-24 17:35:02 +0200

    added styles in InputsToDoList for mobile

--- src/Components/ToDoList/InputsToDoList.tsx
+++ src/Components/ToDoList/InputsToDoList.tsx
@@ -5,27 +5,29 @@ import styles from './Style/InputsToDoList.module.css'
 const InputsToDoList = () => {
 	return (
 		<section className={styles.wrapper_inputs}>
+			<h1 className={styles.heading}>ToDo List</h1>
 			<div className={styles.box_inputs}>
-				<div className={styles.input_text}>
+				<div className={styles.inputs}>
 					<label className={styles.label}>Wpisz zadanie:</label>
 					<input className={styles.input} type='text' />
-					<span className={styles.quantity_signs}>Ilość znaków: 0</span>
-					<span className={styles.quantity_errors}>error</span>
+					<div className={styles.quantity_inputs}>
+						<span className={styles.quantity_signs}>Ilość znaków: 0</span>
+						<span className={styles.quantity_errors}>Osiągąłeś maksymalną ilość znaków</span>
+					</div>
 				</div>
 				<div className={styles.input_important}>
 					<label className={styles.label}>Ważość:</label>
 					<input className={styles.checkbox} type='checkbox' />
 				</div>
+				<button className={styles.btn_added_task}>Dodaj</button>
 			</div>
 			<div className={styles.box_info}>
-				<div className={styles.quantity_toDo}>
-					<h4 className={styles.quantity_heading}>Ilość zadań do wykoania:</h4>
-					<span className={styles.quantity_result}>0</span>
-				</div>
-				<div className={styles.quantity_done}>
-					<h4 className={styles.quantity_heading}>Ilość zadań wykonanych</h4>
-					<span className={styles.quantity_result}>0</span>
-				</div>
+				<h4 className={styles.quantity_heading}>
+					Ilość zadań do wykoania: <span className={styles.quantity_result}>0</span>
+				</h4>
+				<h4 className={styles.quantity_heading}>
+					Ilość zadań wykonanych: <span className={styles.quantity_result}>0</span>
+				</h4>
 			</div>
 		</section>
 	)
