c61072e3fe64ce4f740a679b2a9ebface084eabe
skorpion <jmosinski@o2.pl>
2024-08-25 20:55:28 +0200

    added structure and styles css for component DoneTask

--- src/Components/ToDoList/DoneTask.tsx
+++ src/Components/ToDoList/DoneTask.tsx
@@ -5,7 +5,18 @@ import styles from './Style/DoneTask.module.css'
 const DoneTask = () => {
 	return (
 		<section className={styles.wrapper}>
-			<div></div>
+			<div className={styles.box_content}>
+				<h2 className={styles.heading}>Lista zadań do zrobionych</h2>
+				<div className={styles.box_info}>
+					<span className={styles.info}>Informacja:</span>
+				</div>
+				<div className={styles.box_done}>
+					<p>pierwszy</p>
+					<p>drugi</p>
+					<p>trzeci</p>
+				</div>
+			</div>
+			<button className={styles.main_btn}>Usuń wszystko</button>
 		</section>
 	)
 }
