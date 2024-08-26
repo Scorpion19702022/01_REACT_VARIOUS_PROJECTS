bbcd76b2147d900f8de1d76f69838daf2d91a46e
skorpion <jmosinski@o2.pl>
2024-08-26 18:59:29 +0200

    added view task in component ToDoTask

--- src/Components/ToDoList/ToDoTask.tsx
+++ src/Components/ToDoList/ToDoTask.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import styles from './Style/ToDoTask.module.css'
+import ToDoContext from './Context/ToDoContext'
 
 const ToDoTask = () => {
+	const { toDo } = useContext(ToDoContext)
+
+	const taskToDo = toDo.map(item => (
+		<div key={item.id}>
+			<h5 className={item.important ? styles.title_task_important : styles.title_task_no_important}>{item.title}</h5>
+			<span className={item.important ? styles.date_important : styles.date_no_important}>{item.addDate}</span>
+		</div>
+	))
+
 	return (
 		<section className={styles.wrapper}>
 			<div className={styles.box_content}>
@@ -15,11 +25,7 @@ const ToDoTask = () => {
 					<button className={styles.select_btn}>ważne</button>
 					<button className={styles.select_btn}>inne</button>
 				</div>
-				<div className={styles.box_todo}>
-					<p>pierwszy</p>
-					<p>drugi</p>
-					<p>trzeci</p>
-				</div>
+				<div className={styles.box_todo}>{taskToDo}</div>
 			</div>
 			<button className={styles.main_btn}>Usuń wszystko</button>
 		</section>
