e7ade309fd745ee47114d4b68bcfe5fdab77ab68
skorpion <jmosinski@o2.pl>
2024-08-31 22:59:42 +0200

    added styles in popup for desktop

--- src/Components/ToDoList/ToDoTask.tsx
+++ src/Components/ToDoList/ToDoTask.tsx
@@ -53,12 +53,23 @@ const ToDoTask = () => {
 				<h5 className={item.important ? styles.title_task_important : styles.title_task_no_important}>{item.title}</h5>
 				<span className={item.important ? styles.date_important : styles.date_no_important}>{item.addDate}</span>
 				<div className={styles.btns_task}>
-					<button className={styles.btn_task} onClick={() => handleDeleteTask(item.id, item.title)}>
+					<button className={styles.btn_task} onClick={() => handleSureDelete(item.id)}>
 						Usuń
 					</button>
 					<button className={styles.btn_task} onClick={() => handleDoneTask(item.id, item.title)}>
 						Zrobione
 					</button>
+				</div>
+				<div className={!sureDelete[item.id] ? styles.no_popup : styles.popup}>
+					<h4 className={styles.popup_heading}>Jesteś pewien, że chcesz usunąć zadanie?</h4>
+					<div className={styles.box_btn_popup}>
+						<button className={styles.btn_popup} onClick={() => handleDeleteTask(item.id, item.title)}>
+							Tak
+						</button>
+						<button className={styles.btn_popup} onClick={() => handleSureDelete(item.id)}>
+							Nie
+						</button>
+					</div>
 				</div>
 			</div>
 		))
@@ -70,12 +81,23 @@ const ToDoTask = () => {
 				<h5 className={item.important ? styles.title_task_important : styles.title_task_no_important}>{item.title}</h5>
 				<span className={item.important ? styles.date_important : styles.date_no_important}>{item.addDate}</span>
 				<div className={styles.btns_task}>
-					<button className={styles.btn_task} onClick={() => handleDeleteTask(item.id, item.title)}>
+					<button className={styles.btn_task} onClick={() => handleSureDelete(item.id)}>
 						Usuń
 					</button>
 					<button className={styles.btn_task} onClick={() => handleDoneTask(item.id, item.title)}>
 						Zrobione
 					</button>
+				</div>
+				<div className={!sureDelete[item.id] ? styles.no_popup : styles.popup}>
+					<h4 className={styles.popup_heading}>Jesteś pewien, że chcesz usunąć zadanie?</h4>
+					<div className={styles.box_btn_popup}>
+						<button className={styles.btn_popup} onClick={() => handleDeleteTask(item.id, item.title)}>
+							Tak
+						</button>
+						<button className={styles.btn_popup} onClick={() => handleSureDelete(item.id)}>
+							Nie
+						</button>
+					</div>
 				</div>
 			</div>
 		))
