f3febe329f764d273afd7f43a25e236df3e53a02
skorpion <jmosinski@o2.pl>
2024-09-03 18:59:29 +0200

    added text for component BmiInfo

--- src/Components/Bmi/BmiInfo.tsx
+++ src/Components/Bmi/BmiInfo.tsx
@@ -5,7 +5,15 @@ import styles from './Style/BmiInfo.module.css'
 const BmiInfo = () => {
 	return (
 		<section className={styles.wrapper}>
-			<h1>Informacja</h1>
+			<h1 className={styles.heading}>Czym jest wskaźnik BMI</h1>
+			<p className={styles.text}>
+				Wskaźnik BMI to stosunek masy ciała (wagi) do wzrostu. Niskie wartości mówią o tym, że ważmy za mało, wysokie –
+				że za dużo. Im wyższy współczynnik BMI, tym wyższe ryzyko wystąpienia chorób związanych z nadmierną masą ciała.
+				Są to choroby serca, cukrzyca typu 2, nadciśnienie, miażdżyca, kamicy żółciowej, zapalenia kości i stawów,
+				bezdechów sennych, a nawet niektórych nowotworów. Zbyt niski wskaźnik BMI także wiąże się ze zagrożeniem zdrowia
+				ze względu na niedożywienie, które może prowadzić do zmniejszenia odporności organizmu, apatii, zmniejszenia
+				zdolności motorycznych, objawów niedoborów witamin.
+			</p>
 		</section>
 	)
 }
