8dc74b7c4e55a7834d3d409b06c8cbc33b78a590
skorpion <jmosinski@o2.pl>
2024-09-03 19:52:33 +0200

    added structure for bmiInputs

--- src/Components/Bmi/BmiInputs.tsx
+++ src/Components/Bmi/BmiInputs.tsx
@@ -6,7 +6,23 @@ import styles from './Style/BmiInputs.module.css'
 const BmiInputs = () => {
 	return (
 		<section className={styles.wrapper}>
-			<h1>Inputs</h1>
+			<div className={styles.box_inputs}>
+				<h2>Wprowadź swoje dane</h2>
+				<div className={styles.input_tall}>
+					<label>Podaj wzrost w centymetrach:</label>
+					<input type='number' min={0} />
+				</div>
+				<div className={styles.input_weight}>
+					<div className={styles.input_tall}>
+						<label>Podaj wagę w kilogramach:</label>
+						<input type='number' min={0} />
+					</div>
+				</div>
+				<div className={styles.box_btns}>
+					<button className={styles.btn}>Sprawdź</button>
+					<button className={styles.btn}>Wyczyść</button>
+				</div>
+			</div>
 			<BmiResult />
 		</section>
 	)
