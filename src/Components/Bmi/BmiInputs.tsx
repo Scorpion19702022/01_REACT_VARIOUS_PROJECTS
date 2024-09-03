3f6a880e9d3e6b0b6d193f0e393161e7478c2b4c
skorpion <jmosinski@o2.pl>
2024-09-03 20:57:13 +0200

    added styles for desktop

--- src/Components/Bmi/BmiInputs.tsx
+++ src/Components/Bmi/BmiInputs.tsx
@@ -10,12 +10,18 @@ const BmiInputs = () => {
 				<h2 className={styles.heading}>Wprowadź swoje dane</h2>
 				<div className={styles.input_tall}>
 					<label className={styles.label}>Podaj wzrost w centymetrach:</label>
-					<input className={styles.input} type='number' min={0} />
+					<div className={styles.box_input}>
+						<input className={styles.input} type='number' min={0} />
+						<span className={styles.span_input}>cm</span>
+					</div>
 				</div>
 				<div className={styles.input_weight}>
 					<div className={styles.input_tall}>
 						<label className={styles.label}>Podaj wagę w kilogramach:</label>
-						<input className={styles.input} type='number' min={0} />
+						<div className={styles.box_input}>
+							<input className={styles.input} type='number' min={0} />
+							<span className={styles.span_input}>kg</span>
+						</div>
 					</div>
 				</div>
 				<div className={styles.box_btns}>
