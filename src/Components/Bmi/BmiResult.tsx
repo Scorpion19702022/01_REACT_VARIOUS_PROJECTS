676eab6f567e559d425d9d01e235a3f346421ec9
skorpion <jmosinski@o2.pl>
2024-09-03 21:15:58 +0200

    added structure for bmi result

--- src/Components/Bmi/BmiResult.tsx
+++ src/Components/Bmi/BmiResult.tsx
@@ -2,10 +2,25 @@ import React from 'react'
 
 import styles from './Style/BmiResult.module.css'
 
+import woman from './assets/bmi_4.png'
+
 const BmiResult = () => {
 	return (
 		<section className={styles.wrapper}>
-			<h1>Result</h1>
+			<h2 className={styles.heading}>Poznaj swoje BMI</h2>
+			<div className={styles.box_info_result}>
+				<h4 className={styles.headig_result}>
+					Twoje BMI wynosi: <span className={styles.result}>28</span>
+				</h4>
+				<div className={styles.box_info_result}>
+					<div className={styles.box_text}>
+						<p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, inventore.</p>
+					</div>
+					<div className={styles.box_img}>
+						<img className={styles.img} src={woman} alt='' />
+					</div>
+				</div>
+			</div>
 		</section>
 	)
 }
