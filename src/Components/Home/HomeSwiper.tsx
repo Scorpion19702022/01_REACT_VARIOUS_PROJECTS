b2688eeba472a3aafdb2539c1a11f5e2804eca4a
skorpion <jmosinski@o2.pl>
2024-09-08 19:53:55 +0200

    added swiper in Home

--- src/Components/Home/HomeSwiper.tsx
+++ src/Components/Home/HomeSwiper.tsx
@@ -16,17 +16,55 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules'
 const HomeSwiper = () => {
 	return (
 		<section className={styles.wrapper_card}>
-			<div className={styles.box_card}>
-				<div className={styles.card_box_text}>
-					<h4 className={styles.card_heading}>Aplikacja pogodowa</h4>
-					<p className={styles.card_text}>
-						Aktualna pogoda dla miast Polski i ważniejszych miast na poszczególnych kontyentach. Aktualizacja co 10 miut
-					</p>
-				</div>
-				<div className={styles.card_box_img}>
-					<img className={styles.card_img} src={imgSlider01} alt='photoSlider' />
-				</div>
-			</div>
+			<Swiper
+				slidesPerView={1}
+				spaceBetween={10}
+				pagination={{
+					clickable: true,
+				}}
+				breakpoints={{
+					778: {
+						slidesPerView: 1,
+						spaceBetween: 20,
+					},
+					1024: {
+						slidesPerView: 2,
+						spaceBetween: 40,
+					},
+				}}
+				navigation={true}
+				modules={[Autoplay, Pagination, Navigation]}
+				className={styles.swiper}
+			>
+				<SwiperSlide className={styles.box_swiper}>
+					<div className={styles.box_card_swiper}>
+						<div className={styles.card_box_text}>
+							<h4 className={styles.card_heading}>Aplikacja pogodowa</h4>
+							<p className={styles.card_text}>
+								Aktualna pogoda dla miast Polski i ważniejszych miast na poszczególnych kontyentach. Aktualizacja co 10
+								miut
+							</p>
+						</div>
+						<div className={styles.card_box_img}>
+							<img className={styles.card_img} src={imgSlider01} alt='photoSlider' />
+						</div>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide className={styles.box_swiper}>
+					<div className={styles.box_card_swiper}>
+						<div className={styles.card_box_text}>
+							<h4 className={styles.card_heading}>Aplikacja pogodowa</h4>
+							<p className={styles.card_text}>
+								Aktualna pogoda dla miast Polski i ważniejszych miast na poszczególnych kontyentach. Aktualizacja co 10
+								miut
+							</p>
+						</div>
+						<div className={styles.card_box_img}>
+							<img className={styles.card_img} src={imgSlider02} alt='photoSlider' />
+						</div>
+					</div>
+				</SwiperSlide>
+			</Swiper>
 		</section>
 	)
 }
