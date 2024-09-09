8673c365145d1592dc90f47e5f2f3e82e5fa6029
skorpion <jmosinski@o2.pl>
2024-09-09 21:36:55 +0200

    added styles for mobile

--- src/Components/SalaryCalculator/SalaryInput.tsx
+++ src/Components/SalaryCalculator/SalaryInput.tsx
@@ -8,18 +8,23 @@ const SalaryInput = () => {
 	return (
 		<section className={styles.wrapper}>
 			<h2 className={styles.heading}>Kalkulator wynagrodzenia dotyczy tylko umowy o pracę na pełny etat.</h2>
-			<div className={styles.box_inputs}>
+			<div className={styles.box_main}>
 				<div className={styles.box_input_img}>
 					<img className={styles.input_img} src={imgInput} alt='' />
 				</div>
-				<div className={styles.box_input}>
-					<label className={styles.label}>Wpisz swoje wynagrodzenie brutto</label>
-					<input className={styles.input} type='number' />
+				<div className={styles.box_inputs}>
+					<div className={styles.box_input_label}>
+						<label className={styles.label}>Wpisz wynagrodzenie brutto</label>
+						<div className={styles.box_input}>
+							<input className={styles.input} type='number' />
+							<span className={styles.kind_value}>zł brutto</span>
+						</div>
+					</div>
+					<div className={styles.box_btns}>
+						<button className={styles.btn}>Sprawdź</button>
+						<button className={styles.btn}>Wyczyść</button>
+					</div>
 				</div>
-			</div>
-			<div className={styles.box_btns}>
-				<button className={styles.btn}>Sprawdź</button>
-				<button className={styles.btn}>Wyczyść</button>
 			</div>
 		</section>
 	)
