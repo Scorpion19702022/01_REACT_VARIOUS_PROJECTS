b9e6d0f9881188bbc1252d501639d85a561d8966
skorpion <jmosinski@o2.pl>
2024-09-09 20:39:12 +0200

    added structure for SalaryInput

--- src/Components/SalaryCalculator/SalaryInput.tsx
+++ src/Components/SalaryCalculator/SalaryInput.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import imgInput from './assets/salary.png'
+
+import styles from './Styles/SalaryInput.module.css'
+
 const SalaryInput = () => {
 	return (
-		<section>
-			<h2>input</h2>
+		<section className={styles.wrapper}>
+			<h4 className={styles.heading}>Kalkulator wynagrodzenia dotyczy tylko umowy o pracę na pełny etat.</h4>
+			<div className={styles.box_inputs}>
+				<div className={styles.box_input_img}>
+					<img className={styles.input_img} src={imgInput} alt='' />
+				</div>
+				<div className={styles.box_input}>
+					<label className={styles.label}>Wpisz swoje wynagrodzenie brutto</label>
+					<input className={styles.input} type='number' />
+				</div>
+			</div>
+			<div className={styles.box_btns}>
+				<button className={styles.btn}>Sprawdź</button>
+				<button className={styles.btn}>Wyczyść</button>
+			</div>
 		</section>
 	)
 }
