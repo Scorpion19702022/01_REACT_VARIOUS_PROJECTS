371757d83750e6fb86c8067f20aa408585aa3123
skorpion <jmosinski@o2.pl>
2024-09-10 22:11:45 +0200

    added first style for others componets

--- src/Components/SalaryCalculator/SalaryResult.tsx
+++ src/Components/SalaryCalculator/SalaryResult.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import SalaryTax from './SalaryTax'
 
+import styles from './Styles/SalaryResult.module.css'
+
 const SalaryResult = () => {
 	return (
-		<section>
-			<h2>resultat</h2>
+		<section className={styles.wrapper}>
+			<div className={styles.box_result}>
+				<h2>Box result</h2>
+			</div>
 			<SalaryTax />
 		</section>
 	)
