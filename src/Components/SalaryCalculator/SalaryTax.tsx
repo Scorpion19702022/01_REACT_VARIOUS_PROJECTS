eefa18b0915ba062aefcb3abe6705db5e19d7096
skorpion <jmosinski@o2.pl>
2024-09-11 20:44:46 +0200

    added structure and styles for SalaryTax

--- src/Components/SalaryCalculator/SalaryTax.tsx
+++ src/Components/SalaryCalculator/SalaryTax.tsx
@@ -5,7 +5,37 @@ import styles from './Styles/SalaryTax.module.css'
 const SalaryTax = () => {
 	return (
 		<section className={styles.wrapper}>
-			<div className={styles.box_tax}></div>
+			<h2 className={styles.heading_tax}>Składki</h2>
+			<div className={styles.box_tax}>
+				<div className={styles.tax}>
+					<h4 className={styles.name_tax}>Składka ZUS:</h4>
+					<span className={styles.result_tax}>0 zł</span>
+				</div>
+				<div className={styles.tax}>
+					<h4 className={styles.name_tax}>Składka emerytalna:</h4>
+					<span className={styles.result_tax}>0 zł</span>
+				</div>
+				<div className={styles.tax}>
+					<h4 className={styles.name_tax}>Składka rentowa:</h4>
+					<span className={styles.result_tax}>0 zł</span>
+				</div>
+				<div className={styles.tax}>
+					<h4 className={styles.name_tax}>Składka chorobowa:</h4>
+					<span className={styles.result_tax}>0 zł</span>
+				</div>
+				<div className={styles.tax}>
+					<h4 className={styles.name_tax}>Składka zdrowotna:</h4>
+					<span className={styles.result_tax}>0 zł</span>
+				</div>
+				<div className={styles.tax}>
+					<h4 className={styles.name_tax}>Zaliczka na podatek:</h4>
+					<span className={styles.result_tax}>0 zł</span>
+				</div>
+				<div className={styles.sum_tax}>
+					<h4 className={styles.name_sum_tax}>Składki razem:</h4>
+					<span className={styles.result_sum_tax}>0 zł</span>
+				</div>
+			</div>
 		</section>
 	)
 }
