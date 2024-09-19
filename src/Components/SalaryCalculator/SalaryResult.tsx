03d657be5522cb6c69ed0488ab1468973d148d5f
skorpion <jmosinski@o2.pl>
2024-09-19 20:13:53 +0200

    added popup with information

--- src/Components/SalaryCalculator/SalaryResult.tsx
+++ src/Components/SalaryCalculator/SalaryResult.tsx
@@ -8,6 +8,16 @@ const SalaryResult = () => {
 	const { resultContract, resultGrossSalary, resultNetSalary, handleDeleteAll } = useContext(SalaryContext)
 	return (
 		<section className={styles.wrapper}>
+			<div className={styles.box_info}>
+				<h2 className={styles.heading_info}>Informacja</h2>
+				<p className={styles.text_info}>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos dolorum minus optio obcaecati excepturi
+					similique consequuntur illo, magnam aliquid, perspiciatis laboriosam sunt ullam sint ea, aspernatur rerum
+					mollitia! Molestiae architecto quibusdam quos modi aliquid delectus numquam consectetur non in est quod
+					laudantium ipsum itaque hic animi quae voluptatem, illo consequatur.
+				</p>
+				<button className={styles.btn_close_info}>Zamknij i zobacz obliczenia</button>
+			</div>
 			<div className={styles.box_results}>
 				<div className={styles.result}>
 					<h2 className={styles.heading_small}>Rodzaj umowy:</h2>
