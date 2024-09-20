49a542aa39ddf73e99368dbcf998ef9282015043
skorpion <jmosinski@o2.pl>
2024-09-20 20:36:25 +0200

    added cotent for component poster

--- src/Components/SnackBar/SnackBarPoster.tsx
+++ src/Components/SnackBar/SnackBarPoster.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 
 import styles from './Styles/SnackBarPoster.module.css'
 
+import SnackBarPosterImg from './assets/snack_01.png'
+
 const SnackBarPoster = () => {
 	return (
 		<section className={styles.wrapper}>
 			<div className={styles.box_poster}>
-				<h1 className={styles.poster_heading}>SnackBar</h1>
+				<img className={styles.poster_img} src={SnackBarPosterImg} alt='photo_foods' />
+				<div className={styles.box_info_poster}>
+					<h1 className={styles.poster_heading}>SnackBar</h1>
+					<span className={styles.poster_text}>Pysznie i szybko!</span>
+				</div>
 			</div>
 		</section>
 	)
