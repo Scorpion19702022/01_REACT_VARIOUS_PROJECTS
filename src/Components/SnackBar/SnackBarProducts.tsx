78cb5075c27c67ccaaa81a5507021ff9b4964da8
skorpion <jmosinski@o2.pl>
2024-09-21 21:12:42 +0200

    added button for select menu and styles for mobile

--- src/Components/SnackBar/SnackBarProducts.tsx
+++ src/Components/SnackBar/SnackBarProducts.tsx
@@ -10,7 +10,7 @@ const SnackBarProducts = () => {
 	const productsList = products.map(item => (
 		<div className={styles.product} key={item.id}>
 			<h4 className={styles.product_name}>{item.name}</h4>
-			<span className={styles.product_price}>{item.price}</span>
+			<span className={styles.product_price}>{item.price} zł</span>
 			<div className={styles.box_img_product}>
 				<img className={styles.img_product} src={item.img} alt='' />
 			</div>
@@ -21,7 +21,12 @@ const SnackBarProducts = () => {
 	return (
 		<section className={styles.wrapper}>
 			<div className={styles.box_products}>
-				<h1>Produkty</h1>
+				<h1 className={styles.products_heading}>Nasze menu:</h1>
+				<div className={styles.box_products_btns}>
+					<button className={styles.products_btn}>dania głowne</button>
+					<button className={styles.products_btn}>dodatki i desery</button>
+					<button className={styles.products_btn}>napoje i alkohol</button>
+				</div>
 				<div className={styles.products}>{productsList}</div>
 			</div>
 			<SnackBarOrder />
