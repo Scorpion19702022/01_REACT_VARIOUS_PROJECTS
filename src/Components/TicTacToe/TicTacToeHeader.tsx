2b69393228f15f269f6d396381b4be8aa425a4ac
skorpion <jmosinski@o2.pl>
2024-09-30 20:54:52 +0200

    added styles for mobile

--- src/Components/TicTacToe/TicTacToeHeader.tsx
+++ src/Components/TicTacToe/TicTacToeHeader.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 import styles from './Styles/TicTacToeHeader.module.css'
+import TicTacImg01 from './assets/tic_01.png'
 
 const TicTacToeHeader = () => {
 	return (
 		<section className={styles.wrapper}>
-			<h1>Tic-Tac-Toe</h1>
+			<div className={styles.header_img}>
+				<img className={styles.img} src={TicTacImg01} alt='' />
+			</div>
+			<div className={styles.back}></div>
+			<div className={styles.header_text}>
+				<h1>Tic Tac Toe</h1>
+				<span>zagraj w popularną grę</span>
+			</div>
 		</section>
 	)
 }
