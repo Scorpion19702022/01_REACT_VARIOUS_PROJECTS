3ac6aa4bc6c3a98c8ab4bfee3efc18bfcecf167a
skorpion <jmosinski@o2.pl>
2024-10-01 20:24:37 +0200

    added styles for mobile

--- src/Components/TicTacToe/TicTacToePlayers.tsx
+++ src/Components/TicTacToe/TicTacToePlayers.tsx
@@ -7,13 +7,12 @@ const TicTacToePlayers = () => {
 		<section className={styles.wrapper}>
 			<div className={styles.box_players}>
 				<div className={styles.box_player_one}>
-					<input className={styles.player_input} type='text' placeholder='Player-1' />
-					<span className={styles.player_sign}>X</span>
+					<input className={styles.player_input} type='text' placeholder='Player-X' />
 					<button className={styles.player_save_btn}>edytuj</button>
 				</div>
+				<div className={styles.line_select}></div>
 				<div className={styles.box_player_two}>
-					<input className={styles.player_input} type='text' placeholder='Player-2' />
-					<span className={styles.player_sign}>O</span>
+					<input className={styles.player_input} type='text' placeholder='Player-0' />
 					<button className={styles.player_save_btn}>edytuj</button>
 				</div>
 			</div>
