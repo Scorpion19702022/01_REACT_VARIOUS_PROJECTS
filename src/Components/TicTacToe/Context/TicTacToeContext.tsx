4c56cff372b71d820ad6bd8c869a566a886c2580
skorpion <jmosinski@o2.pl>
2024-10-05 18:23:20 +0200

    added styles for compoets board game

--- src/Components/TicTacToe/Context/TicTacToeContext.tsx
+++ src/Components/TicTacToe/Context/TicTacToeContext.tsx
@@ -1,17 +1,27 @@
 import { Analytics } from '@vercel/analytics/react'
 import React, { createContext, useState } from 'react'
 
-type InitialStateType = {}
+type InitialStateType = {
+	initialGameBorder: any[]
+}
 
 type TicTacToeProviderType = { children: React.ReactNode }
 
-const InitialState: InitialStateType = {}
+const InitialState: InitialStateType = {
+	initialGameBorder: [[], [], []],
+}
 
 const TicTacToeContext = createContext(InitialState)
 
 export const TicTacToeProvider = ({ children }: TicTacToeProviderType) => {
+	const [initialGameBorder, setInitialGameBorder] = useState<any[]>([
+		[null, null, null],
+		[null, null, null],
+		[null, null, null],
+	])
+
 	return (
-		<TicTacToeContext.Provider value={{}}>
+		<TicTacToeContext.Provider value={{ initialGameBorder }}>
 			{children} <Analytics />
 		</TicTacToeContext.Provider>
 	)
--- src/Components/TicTacToe/TicTacToeGameBoard.tsx
+++ src/Components/TicTacToe/TicTacToeGameBoard.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import styles from './Styles/TicTacToeGameBoard.module.css'
+import TicTacToeContext from './Context/TicTacToeContext'
 
 const TicTacToeGameBoard = () => {
+	const { initialGameBorder } = useContext(TicTacToeContext)
+
+	console.log(initialGameBorder)
+
 	return (
 		<section className={styles.wrapper}>
-			<div className={styles.box_game_board}></div>
+			<div className={styles.box_game_board}>
+				<ol className={styles.game_board}>
+					{initialGameBorder.map((row, rowIndex) => (
+						<li key={rowIndex} className={styles.game_row}>
+							<ol className={styles.game_group}>
+								{row.map((playerSymbol: any, colIndex: any) => (
+									<li key={colIndex} className={styles.game_col}>
+										<button className={styles.game_btns}>{playerSymbol}</button>
+									</li>
+								))}
+							</ol>
+						</li>
+					))}
+				</ol>
+			</div>
 		</section>
 	)
 }
