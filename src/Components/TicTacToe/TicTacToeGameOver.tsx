075b78e4a857946630cdbd3c393c224a0e5dfe5b
skorpion <jmosinski@o2.pl>
2024-10-08 21:49:52 +0200

    added service for gameOver

--- src/Components/TicTacToe/TicTacToeGameOver.tsx
+++ src/Components/TicTacToe/TicTacToeGameOver.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import styles from './Styles/GameOver.module.css'
+import TicTacToeContext from './Context/TicTacToeContext'
 
 export type InitialTypeProps = {
 	winner: string | undefined
@@ -8,10 +9,12 @@ export type InitialTypeProps = {
 }
 
 const TicTacToeGameOver: React.FC<InitialTypeProps> = ({ winner, restartGame }) => {
+	const { playerNames } = useContext(TicTacToeContext)
+
 	return (
 		<div className={styles.box_game_over}>
 			<h2 className={styles.game_over_heading}>Gra zakończona</h2>
-			{winner && <p>Zwyciężył: {winner}</p>}
+			{winner && <p>Zwyciężył: {playerNames[winner as 'X' | 'O']}</p>}
 			{!winner && <p>Gra nie rozstrzygnięta</p>}
 			<button onClick={restartGame}>Restart</button>
 		</div>
