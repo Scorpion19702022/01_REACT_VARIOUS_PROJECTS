3897d9092e4a8de693499605230e745b6434b125
skorpion <jmosinski@o2.pl>
2024-10-11 20:28:06 +0200

    added data and type in hook

--- src/Components/Quiz/Hook/useQuiz.tsx
+++ src/Components/Quiz/Hook/useQuiz.tsx
@@ -1,8 +1,15 @@
+type Question = {
+	id: number
+	question: string
+	img: string
+	answers: { answerText: string; isCorrect: boolean }[]
+}
+
 const useQuiz = () => {
-	const QuizList = [
+	const QuizList: Question[] = [
 		{
 			id: 0,
-			question: 'Misto z Pałacem Kultury i Pałacem Królewskim to?',
+			question: 'Miasto z Pałacem Kultury i Pałacem Królewskim to?',
 			img: 'https://cdn.pixabay.com/photo/2017/09/11/17/26/warsaw-2739767_960_720.jpg',
 			answers: [
 				{ answerText: 'Berlin', isCorrect: false },
@@ -13,7 +20,7 @@ const useQuiz = () => {
 		},
 		{
 			id: 1,
-			question: 'Tu obejrzysz korridę, tańce flamenco i pójdziesz na mecz słynego klubu',
+			question: 'Tu obejrzysz korridę, tańce flamenco i pójdziesz na mecz słynnego klubu',
 			img: 'https://cdn.pixabay.com/photo/2015/10/12/23/57/spain-985372_960_720.jpg',
 			answers: [
 				{ answerText: 'Madryt', isCorrect: true },
