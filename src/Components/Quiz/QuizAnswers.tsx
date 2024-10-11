5563f71c242697723fa2a8ebb779bb7a828a84a2
skorpion <jmosinski@o2.pl>
2024-10-11 20:35:50 +0200

    corrected style in header component for desktop

--- src/Components/Quiz/QuizAnswers.tsx
+++ src/Components/Quiz/QuizAnswers.tsx
@@ -3,18 +3,11 @@ import React, { useState } from 'react'
 import styles from './Styles/QuizAnswers.module.css'
 
 const QuizAnswers = () => {
-	const [pro, setPro] = useState(10)
-
-	const handleProgress = () => {
-		setPro(pro + 10)
-	}
-
 	return (
 		<section className={styles.wrapper}>
 			<div className={styles.box_progress}>
-				<progress className={styles.progress} max='100' value={pro}></progress>
+				<progress className={styles.progress} max='100' value={10}></progress>
 			</div>
-			<button onClick={handleProgress}>PROGRESS</button>
 		</section>
 	)
 }
