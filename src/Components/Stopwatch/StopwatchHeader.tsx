8e5e4917fe67542d159582dca3180ab9d7f4df5f
skorpion <jmosinski@o2.pl>
2024-10-18 19:49:58 +0200

    added style for mobile

--- src/Components/Stopwatch/StopwatchHeader.tsx
+++ src/Components/Stopwatch/StopwatchHeader.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 import styles from './Styles/StopwatchHeader.module.css'
+import Watch from './assets/stopwatch.png'
 
 const StopwatchHeader = () => {
 	return (
 		<section className={styles.wrapper}>
-			<h1>Stoper</h1>
+			<h1 className={styles.heading}>
+				St{' '}
+				<span className={styles.box_img}>
+					<img className={styles.img} src={Watch} alt='stopwatch_picture' />
+				</span>
+				per
+			</h1>
 		</section>
 	)
 }
