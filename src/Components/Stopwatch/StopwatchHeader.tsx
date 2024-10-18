937105565712720f309215f35962104f0be847d4
skorpion <jmosinski@o2.pl>
2024-10-18 20:16:51 +0200

    added styles for desktop

--- src/Components/Stopwatch/StopwatchHeader.tsx
+++ src/Components/Stopwatch/StopwatchHeader.tsx
@@ -6,13 +6,15 @@ import Watch from './assets/stopwatch.png'
 const StopwatchHeader = () => {
 	return (
 		<section className={styles.wrapper}>
-			<h1 className={styles.heading}>
-				St{' '}
-				<span className={styles.box_img}>
-					<img className={styles.img} src={Watch} alt='stopwatch_picture' />
-				</span>
-				per
-			</h1>
+			<div className={styles.box_header}>
+				<h1 className={styles.heading}>
+					St{' '}
+					<span className={styles.box_img}>
+						<img className={styles.img} src={Watch} alt='stopwatch_picture' />
+					</span>
+					per
+				</h1>
+			</div>
 		</section>
 	)
 }
