c7976914c70669ed2a3bb22c48ccb1f54e7027ef
skorpion <jmosinski@o2.pl>
2024-10-18 20:24:10 +0200

    added styles

--- src/Components/Stopwatch/StopwatchHeader.tsx
+++ src/Components/Stopwatch/StopwatchHeader.tsx
@@ -8,11 +8,11 @@ const StopwatchHeader = () => {
 		<section className={styles.wrapper}>
 			<div className={styles.box_header}>
 				<h1 className={styles.heading}>
-					St{' '}
+					ST{' '}
 					<span className={styles.box_img}>
 						<img className={styles.img} src={Watch} alt='stopwatch_picture' />
 					</span>
-					per
+					PER
 				</h1>
 			</div>
 		</section>
