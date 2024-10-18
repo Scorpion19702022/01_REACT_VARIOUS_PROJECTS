08ef981632a5aaba5d3db1d81f18fd9e0a25d8c0
skorpion <jmosinski@o2.pl>
2024-10-18 21:13:10 +0200

    added component StopWatchCountBox

--- src/Components/Stopwatch/Stopwatch.tsx
+++ src/Components/Stopwatch/Stopwatch.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import styles from './Styles/Stopwatch.module.css'
 import StopwatchHeader from './StopwatchHeader'
 import { StopwatchProvider } from './Context/StopwatchContext'
+import StopwatchCountBox from './StopwatchCountBox'
 
 const Stopwatch = () => {
 	return (
 		<main className={styles.wrapper}>
 			<StopwatchProvider>
 				<StopwatchHeader />
+				<StopwatchCountBox />
 			</StopwatchProvider>
 		</main>
 	)
---
+++ src/Components/Stopwatch/StopwatchCountBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Styles/StopwatchCountBox.module.css'
+
+const StopwatchCoutBox = () => {
+	return (
+		<section className={styles.wrapper}>
+			<h2>Stoper</h2>
+		</section>
+	)
+}
+
+export default StopwatchCoutBox
