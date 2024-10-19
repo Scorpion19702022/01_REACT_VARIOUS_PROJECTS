5e7c5405773965af4a8be7eecbea13d1f9f37af5
skorpion <jmosinski@o2.pl>
2024-10-19 17:09:24 +0200

    added icons for buttos control

--- src/Components/Stopwatch/StopwatchCountBox.tsx
+++ src/Components/Stopwatch/StopwatchCountBox.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { AiFillInfoCircle } from 'react-icons/ai'
 import { IoColorPalette } from 'react-icons/io5'
+import { FaPlay } from 'react-icons/fa6'
+import { FaPause } from 'react-icons/fa6'
+import { FaStop } from 'react-icons/fa6'
+import { ImCross } from 'react-icons/im'
 
 import styles from './Styles/StopwatchCountBox.module.css'
 
@@ -15,10 +19,18 @@ const StopwatchCoutBox = () => {
 				<span className={styles.count}>0:00</span>
 			</div>
 			<div className={styles.box_control}>
-				<button className={styles.btn_control}></button>
-				<button className={styles.btn_control}></button>
-				<button className={styles.btn_control}></button>
-				<button className={styles.btn_control}></button>
+				<button className={styles.btn_control}>
+					<FaPlay />
+				</button>
+				<button className={styles.btn_control}>
+					<FaPause />
+				</button>
+				<button className={styles.btn_control}>
+					<FaStop />
+				</button>
+				<button className={styles.btn_control}>
+					<ImCross />
+				</button>
 			</div>
 		</section>
 	)
