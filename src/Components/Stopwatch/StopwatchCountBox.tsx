03574855fe740c99b41ef9ad6a2654ad9c54eaa4
skorpion <jmosinski@o2.pl>
2024-10-19 18:44:59 +0200

    added modal for info

--- src/Components/Stopwatch/StopwatchCountBox.tsx
+++ src/Components/Stopwatch/StopwatchCountBox.tsx
@@ -14,9 +14,31 @@ const StopwatchCoutBox = () => {
 			<div className={styles.box_header}>
 				<h4 className={styles.state_info}> STAN</h4>
 				<div className={styles.box_icons}>
-					<AiFillInfoCircle className={styles.icon_info} />
-					<IoColorPalette className={styles.icon_info} />
+					<button className={styles.btn_icon}>
+						<AiFillInfoCircle className={styles.icon_info} />
+					</button>
+					<button className={styles.btn_icon}>
+						<IoColorPalette className={styles.icon_info} />
+					</button>
 				</div>
+			</div>
+			<div className={styles.box_modal_info}>
+				<h3 className={styles.heading_info}>Instrukcja:</h3>
+				<div className={styles.box_text_info}>
+					<p className={styles.text_info}>
+						<FaPlay /> <span className={styles.span_info}> - start</span>
+					</p>
+					<p className={styles.text_info}>
+						<FaPause /> <span className={styles.span_info}> - pauza</span>
+					</p>
+					<p className={styles.text_info}>
+						<FaStop /> <span className={styles.span_info}> - stop</span>
+					</p>
+					<p className={styles.text_info}>
+						<ImCross /> <span className={styles.span_info}> - skazuj wszystko</span>
+					</p>
+				</div>
+				<button className={styles.btn_close_modal_info}>zamknij</button>
 			</div>
 			<div className={styles.box_count}>
 				<span className={styles.count}>0:00</span>
