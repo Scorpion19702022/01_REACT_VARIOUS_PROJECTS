e77db7eb47d52d55b270df652f76100068ed834d
skorpion <jmosinski@o2.pl>
2024-10-19 16:52:39 +0200

    added icons box count and style for mobile

--- src/Components/Stopwatch/StopwatchCountBox.tsx
+++ src/Components/Stopwatch/StopwatchCountBox.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { AiFillInfoCircle } from 'react-icons/ai'
+import { IoColorPalette } from 'react-icons/io5'
 
 import styles from './Styles/StopwatchCountBox.module.css'
 
 const StopwatchCoutBox = () => {
 	return (
 		<section className={styles.wrapper}>
-			<h2>Stoper</h2>
+			<div className={styles.box_icons}>
+				<AiFillInfoCircle className={styles.icon_info} />
+				<IoColorPalette className={styles.icon_info} />
+			</div>
+			<div className={styles.box_count}>
+				<span className={styles.count}>0:00</span>
+			</div>
 		</section>
 	)
 }
