21abcbb6e31d15d11cc4661b87491905ea66a063
skorpion <jmosinski@o2.pl>
2024-10-20 18:19:31 +0200

    added change colors

--- src/Components/Stopwatch/Context/StopwatchContext.tsx
+++ src/Components/Stopwatch/Context/StopwatchContext.tsx
@@ -44,7 +44,37 @@ export const StopwatchProvider = ({ children }: StopwatchTypeProvider) => {
 
 		const root = document.documentElement
 
-		console.log(root)
+		if (color === 'blue') {
+			root.style.setProperty('--color-theme01', '#d6ecf3')
+			root.style.setProperty('--color-theme02', '#add9e7')
+			root.style.setProperty('--color-theme03', '#85c6db')
+			root.style.setProperty('--color-theme04', '#5cb3cf')
+			root.style.setProperty('--color-theme05', '#34a0c4')
+			root.style.setProperty('--color-theme06', '#29809c')
+			root.style.setProperty('--color-theme07', '#1f6075')
+			root.style.setProperty('--color-theme08', '#14404e')
+			root.style.setProperty('--color-theme09', '#0a2027')
+		} else if (color === 'purple') {
+			root.style.setProperty('--color-theme01', '#f1d6ec')
+			root.style.setProperty('--color-theme02', '#e4adda')
+			root.style.setProperty('--color-theme03', '#d784c8')
+			root.style.setProperty('--color-theme04', '#8c5382')
+			root.style.setProperty('--color-theme05', '#702963')
+			root.style.setProperty('--color-theme06', '#59204f')
+			root.style.setProperty('--color-theme07', '#43183b')
+			root.style.setProperty('--color-theme08', '#2c1027')
+			root.style.setProperty('--color-theme09', '#210c1d')
+		} else if (color === 'green') {
+			root.style.setProperty('--color-theme01', '#e0f5cc')
+			root.style.setProperty('--color-theme02', '#c1eb99')
+			root.style.setProperty('--color-theme03', '#a3e166')
+			root.style.setProperty('--color-theme04', '#84d732')
+			root.style.setProperty('--color-theme05', '#66cd00')
+			root.style.setProperty('--color-theme06', '#51a400')
+			root.style.setProperty('--color-theme07', '#3d7b00')
+			root.style.setProperty('--color-theme08', '#285200')
+			root.style.setProperty('--color-theme09', '#142900')
+		}
 	}
 
 	return (
