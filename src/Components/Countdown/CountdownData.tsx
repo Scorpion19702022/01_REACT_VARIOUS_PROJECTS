a5c8db01feb0755f2dac087da7452b00edeb5a77
skorpion <jmosinski@o2.pl>
2024-10-29 20:13:04 +0100

    added style for mobile

--- src/Components/Countdown/CountdownData.tsx
+++ src/Components/Countdown/CountdownData.tsx
@@ -48,7 +48,8 @@ const CountdownData = () => {
 					<input className={styles.input_date} type='date' />
 				</div>
 				<div className={styles.box_your_event}>
-					<span className={styles.your_event}>{!eventEmpty ? eventError : event}</span>
+					<span className={styles.your_event}>Zdarzenie: {!eventEmpty ? eventError : event}</span>
+					<span className={styles.your_event_date}>Data zdarzenia:</span>
 				</div>
 			</div>
 		</section>
