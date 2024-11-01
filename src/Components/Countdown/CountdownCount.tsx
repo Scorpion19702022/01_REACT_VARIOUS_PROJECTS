61998b09dd118a349af235f690f2d44c4c112e14
skorpion <jmosinski@o2.pl>
2024-11-01 19:11:17 +0100

    added style for mobile

--- src/Components/Countdown/CountdownCount.tsx
+++ src/Components/Countdown/CountdownCount.tsx
@@ -5,7 +5,24 @@ import styles from './Styles/CountdownCount.module.css'
 const CountdownCount = () => {
 	return (
 		<section className={styles.wrapper}>
-			<div></div>
+			<div className={styles.box_count}>
+				<div className={styles.count}>
+					<h4 className={styles.count_heading}>dni</h4>
+					<span className={styles.count_timer}>0</span>
+				</div>
+				<div className={styles.count}>
+					<h4 className={styles.count_heading}>godziny</h4>
+					<span className={styles.count_timer}>0</span>
+				</div>
+				<div className={styles.count}>
+					<h4 className={styles.count_heading}>minuty</h4>
+					<span className={styles.count_timer}>0</span>
+				</div>
+				<div className={styles.count}>
+					<h4 className={styles.count_heading}>sekundy</h4>
+					<span className={styles.count_timer}>0</span>
+				</div>
+			</div>
 		</section>
 	)
 }
