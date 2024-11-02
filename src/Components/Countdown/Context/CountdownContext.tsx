9216e7d4686eada534471e1c6a605058847c04b9
skorpion <jmosinski@o2.pl>
2024-11-02 19:47:39 +0100

    added count time

--- src/Components/Countdown/Context/CountdownContext.tsx
+++ src/Components/Countdown/Context/CountdownContext.tsx
@@ -73,6 +73,13 @@ export const CountdownProvider = ({ children }: CountdownTypeProvider) => {
 				return
 			}
 
+			const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+			const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+			const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+			const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+			setCountdown({ days, hours, minutes, seconds })
+
 			console.log(distance)
 		}, 1000)
 
