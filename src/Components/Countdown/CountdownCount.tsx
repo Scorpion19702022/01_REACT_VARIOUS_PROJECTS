1d68b2b7af942e2b61b973089e25c4ddb50d3ad8
skorpion <jmosinski@o2.pl>
2024-11-03 16:01:59 +0100

    added popup

--- src/Components/Countdown/CountdownCount.tsx
+++ src/Components/Countdown/CountdownCount.tsx
@@ -8,6 +8,19 @@ const CountdownCount = () => {
 
 	return (
 		<section className={styles.wrapper}>
+			<div className={styles.popup}>
+				<h2 className={styles.popup_heading}>UWAGA!!!</h2>
+				<p className={styles.popup_text}>
+					Czas jest liczony od godziny 00:00 dnia następnego. Jeżeli chcesz sprawdzić ile pozostało czasu do końca
+					bierzącego dnia wybierz datę dnia kolejnego. W przypadku innego czasu w przyszłości czas będzie odliczany od
+					rozpoczęcia wybranego dnia.
+				</p>
+				<p className={styles.popup_text}>
+					Odliczanie pokazuje i odlicza w dniach i godzinach łączny czas do rozpoczęcia dnia ze zdarzeniem. Minuty i
+					sekundy pokazują rzeczywiste odliczanie.
+				</p>
+				<button className={styles.popup_btn}>zamknij</button>
+			</div>
 			<div className={styles.box_heading}>
 				<h2 className={styles.heading}>Pozostało:</h2>
 				<h2 className={styles.heading}>Odliczanie minut i sekund:</h2>
