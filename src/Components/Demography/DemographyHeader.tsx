07bbacbdc60b893b46a20173784362107cd3511f
skorpion <jmosinski@o2.pl>
2024-11-08 19:47:27 +0100

    added image ad style for mobile

--- src/Components/Demography/DemographyHeader.tsx
+++ src/Components/Demography/DemographyHeader.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 import styles from './Styles/DemographyHeader.module.css'
 
+import arrow from './assets/arrow.png'
+
 const DemographyHeader = () => {
 	return (
 		<section className={styles.wrapper}>
-			{/* <div className={styles.header_img}>
-				<img className={styles.img} src={QuizLogo} alt='logo' />
-			</div> */}
+			<div className={styles.header_img}>
+				<img className={styles.img} src={arrow} alt='logo' />
+			</div>
 			<div className={styles.back}></div>
 			<div className={styles.header_text}>
 				<h1 className={styles.header}>Demografia</h1>
-				<span className={styles.text}>przyrost ludności najważniejszych miast Polski</span>
+				<span className={styles.text}>przyrost ludności miast Polski</span>
 			</div>
 		</section>
 	)
