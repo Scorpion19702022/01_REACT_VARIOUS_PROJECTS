56b12117b63fd83bbd419511c98361b227f0dada
skorpion <jmosinski@o2.pl>
2024-11-08 19:06:32 +0100

    start project demography

---
+++ src/Components/Demography/Demography.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './Styles/Demography.module.css'
+
+const Demography = () => {
+	return (
+		<main className={styles.wrapper}>
+			<div>
+				<h1>Demografia</h1>
+			</div>
+		</main>
+	)
+}
+
+export default Demography
