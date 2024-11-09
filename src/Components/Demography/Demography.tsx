ae92625a83557bd7474bcff816cf33dae33f7ab5
skorpion <jmosinski@o2.pl>
2024-11-09 15:57:36 +0100

    prepared component for select

--- src/Components/Demography/Demography.tsx
+++ src/Components/Demography/Demography.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import styles from './Styles/Demography.module.css'
 import DemographyHeader from './DemographyHeader'
+import DemographySelect from './DemographySelect'
 
 const Demography = () => {
 	return (
 		<main className={styles.wrapper}>
 			<DemographyHeader />
+			<DemographySelect />
 		</main>
 	)
 }
---
+++ src/Components/Demography/DemographySelect.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './Styles/DemographySelect.module.css'
+
+const DemographySelect = () => {
+	return (
+		<section className={styles.wrapper}>
+			<div className={styles.box_select}>
+				<h1>select</h1>
+			</div>
+		</section>
+	)
+}
+
+export default DemographySelect
