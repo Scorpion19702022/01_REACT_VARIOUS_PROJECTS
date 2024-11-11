e6c530026453e24b63a7a55a969d66030329da34
skorpion <jmosinski@o2.pl>
2024-11-11 20:19:37 +0100

    added data

--- src/Components/Demography/Hook/useCitiesDemography.tsx
+++ src/Components/Demography/Hook/useCitiesDemography.tsx
@@ -66,7 +66,8 @@ const useCitiesDemography = () => {
 			id: 4,
 			city: 'Katowice',
 			img: 'https://cdn.pixabay.com/photo/2017/04/03/17/46/city-2199041_960_720.jpg',
-			description: 'Prawa miejskie Gorzów Wielkopolski otrzymał w 1257 roku.',
+			description:
+				'Katowice powstały jako wieś zagrodnicza pod koniec XVI wieku pod nazwą Kuźnica Bogucka. Prawa miejskie otrzymały 11 września 1865 roku.',
 			population: {
 				'1900': 31738,
 				'1925': 112657,
@@ -74,6 +75,50 @@ const useCitiesDemography = () => {
 				'1975': 343723,
 				'2000': 330625,
 				'2023': 279200,
+			},
+		},
+		{
+			id: 5,
+			city: 'Kielce',
+			img: 'https://cdn.pixabay.com/photo/2018/10/27/15/30/kielce-3776754_960_720.jpg',
+			description:
+				'Kielce prawa miejskie otrzymały przed 1259 rokiem. Będąc niewielką osadą pozostawały jednak na uboczu historii aż do XVIII wieku.',
+			population: {
+				'1900': 14270,
+				'1925': 41346,
+				'1950': 61332,
+				'1975': 151185,
+				'2000': 210956,
+				'2023': 196335,
+			},
+		},
+		{
+			id: 6,
+			city: 'Kraków',
+			img: 'https://cdn.pixabay.com/photo/2019/08/29/19/31/krakow-4439817_960_720.jpg',
+			description:
+				'Kraków jest jednym z najstarszych miast Polski. Do 1795 roku był formalnie stolicą Polski, a do 1611 roku siedzibą władców państwa polskiego. Prawa miejskie przed 1228 rokiem.',
+			population: {
+				'1900': 85300,
+				'1925': 206809,
+				'1950': 343638,
+				'1975': 684600,
+				'2000': 758715,
+				'2023': 807600,
+			},
+		},
+		{
+			id: 7,
+			city: 'Lublin',
+			img: 'https://cdn.pixabay.com/photo/2015/10/18/12/00/view-of-the-city-994220_960_720.jpg',
+			description: 'Dane w trakcie uzupełniania',
+			population: {
+				'1900': 0,
+				'1925': 0,
+				'1950': 0,
+				'1975': 0,
+				'2000': 0,
+				'2023': 0,
 			},
 		},
 	]
