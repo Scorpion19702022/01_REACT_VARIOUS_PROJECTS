937bcf85dbe5eeb4be206f385d7571de49e84f35
skorpion <jmosinski@o2.pl>
2024-11-16 18:33:19 +0100

    change object for array

--- src/Components/Demography/DemographyChart.tsx
+++ src/Components/Demography/DemographyChart.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import styles from './Styles/DemographyChart.module.css'
 
+import { Line } from 'react-chartjs-2'
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+} from 'chart.js'
+import DemographyContext from './Context/DemographyContext'
+
 const DemographyChart = () => {
+	const { selectCity, cityDemography } = useContext(DemographyContext)
+
+	const dataPopulation = Object.entries(cityDemography[0].population)
+
+	console.log(dataPopulation)
+
+	ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+
 	return (
 		<section className={styles.wrapper}>
 			<h2>Wykresy</h2>
