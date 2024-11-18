e586ec11bb571b874245af263228cc257c364bd6
skorpion <jmosinski@o2.pl>
2024-11-18 18:51:22 +0100

    start next project

---
+++ src/Components/Conventer/Conventer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Styles/Conventer.module.css'
+
+const Conventer = () => {
+	return (
+		<main className={styles.wrapper}>
+			<div>Konwenter</div>
+		</main>
+	)
+}
+
+export default Conventer
