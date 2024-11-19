6d65f85038e3f260b3f78b744a8c720cd7e17650
skorpion <jmosinski@o2.pl>
2024-11-19 19:13:04 +0100

    added icon and corrected style

--- src/Components/Conventer/ConverterHeader.tsx
+++ src/Components/Conventer/ConverterHeader.tsx
@@ -5,6 +5,9 @@ import styles from './Styles/ConverterHeader.module.css'
 import thermometrC from './assets/thermometer_C.png'
 import thermometrF from './assets/thermometer_F.png'
 
+import { RiCelsiusFill } from 'react-icons/ri'
+import { RiFahrenheitFill } from 'react-icons/ri'
+
 const ConverterHeader = () => {
 	return (
 		<section className={styles.wrapper}>
@@ -14,6 +17,8 @@ const ConverterHeader = () => {
 			<div className={styles.header_img_two}>
 				<img className={styles.img_two} src={thermometrF} alt='photo_thermometr' />
 			</div>
+			<RiCelsiusFill className={styles.icon_c} />
+			<RiFahrenheitFill className={styles.icon_f} />
 			<div className={styles.back}></div>
 			<div className={styles.header_text}>
 				<h1 className={styles.header}>Konwerter stopni</h1>
