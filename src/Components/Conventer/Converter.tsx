36c9c8c3b98dd0f54b6f7f216381fd15561a24c6
skorpion <jmosinski@o2.pl>
2024-11-19 19:31:57 +0100

    added component

--- src/Components/Conventer/Converter.tsx
+++ src/Components/Conventer/Converter.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import styles from './Styles/Converter.module.css'
 import ConverterHeader from './ConverterHeader'
+import ConverterResult from './ConverterResult'
 
 const Converter = () => {
 	return (
 		<main className={styles.wrapper}>
 			<ConverterHeader />
+			<ConverterResult />
 		</main>
 	)
 }
---
+++ src/Components/Conventer/ConverterResult.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Styles/ConverterResult.module.css'
+
+const ConverterResult = () => {
+	return (
+		<section className={styles.wrapper}>
+			<div></div>
+		</section>
+	)
+}
+
+export default ConverterResult
