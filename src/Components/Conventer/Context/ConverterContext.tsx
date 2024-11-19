0f607b17fbf918b6c444eee0ba53a0b9d537afab
skorpion <jmosinski@o2.pl>
2024-11-19 20:56:27 +0100

    added states in context

--- src/Components/Conventer/Context/ConverterContext.tsx
+++ src/Components/Conventer/Context/ConverterContext.tsx
@@ -1,19 +1,43 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 import { Analytics } from '@vercel/analytics/react'
 
-type InitialStateType = {}
+type InitialStateType = {
+	celsius: string
+	fare: string
+	valueInput: string | null
+	error: boolean
+	isError: string
+	handleChangeInput: (e: string) => void
+}
 
 type ConverterTypeProvider = {
 	children: React.ReactNode
 }
 
-const InitialState: InitialStateType = {}
+const InitialState: InitialStateType = {
+	celsius: '',
+	fare: '',
+	valueInput: '',
+	error: false,
+	isError: '',
+	handleChangeInput: (e: string) => {},
+}
 
 const ConverterContext = createContext(InitialState)
 
 export const ConverterProvider = ({ children }: ConverterTypeProvider) => {
+	const [celsius, setCelsius] = useState<string>('℃')
+	const [fare, setFare] = useState<string>('℉')
+	const [valueInput, setValueInput] = useState('')
+	const [error, setError] = useState<boolean>(false)
+	const [isError, setIsError] = useState<string>('')
+
+	const handleChangeInput = (e: string) => {
+		setValueInput(e)
+	}
+
 	return (
-		<ConverterContext.Provider value={{}}>
+		<ConverterContext.Provider value={{ celsius, fare, valueInput, error, isError, handleChangeInput }}>
 			<Analytics />
 			{children}
 		</ConverterContext.Provider>
