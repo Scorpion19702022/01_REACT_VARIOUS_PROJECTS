8c4c273bfc47982798fa199bf671b3313575b9c9
skorpion <jmosinski@o2.pl>
2024-11-24 16:19:25 +0100

    added component header

--- src/Components/ExchangeRate/ExchangeRateHeader.tsx
+++ src/Components/ExchangeRate/ExchangeRateHeader.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import styles from './Styles/ExchangeRateHeader.module.css'
+
+import Money from './assets/money.png'
+
 const ExchangeRateHeader = () => {
 	return (
-		<section>
-			<div>Header</div>
+		<section className={styles.wrapper}>
+			<div className={styles.header_img}>
+				<img className={styles.img} src={Money} alt='logo_time' />
+			</div>
+			<div className={styles.header_text}>
+				<h1 className={styles.header}>Kurs złotego</h1>
+				<span className={styles.text}>Aktualny kurs złotego do światowych walut</span>
+			</div>
 		</section>
 	)
 }
