3d9719ca64e6ea1c93f5b035332e2d68f8a1a97c
skorpion <jmosinski@o2.pl>
2024-11-25 20:10:26 +0100

    added button for refresh

--- src/Components/ExchangeRate/ExchangeRateCurrentData.tsx
+++ src/Components/ExchangeRate/ExchangeRateCurrentData.tsx
@@ -23,6 +23,7 @@ const ExchangeRateCurrentData = () => {
 			<div className={styles.box_info}>
 				<h1 className={styles.header}>Kursy walut względem złotego</h1>
 				<p className={styles.info}>Aktualizacja: {currentDate} około godziny 12:00</p>
+				<button className={styles.btn_refresh}>odśwież dane</button>
 			</div>
 			<div className={styles.box_exchange_rate}>{currentExchangeRate}</div>
 		</section>
