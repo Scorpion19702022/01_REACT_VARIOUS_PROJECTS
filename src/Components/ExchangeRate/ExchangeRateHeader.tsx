5f1acef39fcfd570e4b2b099bdec7f0438573ef8
skorpion <jmosinski@o2.pl>
2024-11-26 19:17:56 +0100

    added picture

--- src/Components/ExchangeRate/ExchangeRateHeader.tsx
+++ src/Components/ExchangeRate/ExchangeRateHeader.tsx
@@ -6,9 +6,12 @@ import { MdOutlineEuro } from 'react-icons/md'
 import { MdCurrencyPound } from 'react-icons/md'
 import { MdCurrencyFranc } from 'react-icons/md'
 
+import Hand from './assets/hand.png'
+
 const ExchangeRateHeader = () => {
 	return (
 		<section className={styles.wrapper}>
+			<img className={styles.img} src={Hand} alt='picture_hand' />
 			<BiDollar className={styles.icon_dollar} />
 			<MdOutlineEuro className={styles.icon_euro} />
 			<MdCurrencyPound className={styles.icon_pound} />
