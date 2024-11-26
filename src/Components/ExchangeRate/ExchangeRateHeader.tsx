e648577ba1515fb7285350635860144fe4368bd1
skorpion <jmosinski@o2.pl>
2024-11-26 18:45:07 +0100

    change image for icons and added styles for mobile

--- src/Components/ExchangeRate/ExchangeRateHeader.tsx
+++ src/Components/ExchangeRate/ExchangeRateHeader.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 import styles from './Styles/ExchangeRateHeader.module.css'
-
-import Money from './assets/money.png'
+import { BiDollar } from 'react-icons/bi'
+import { MdOutlineEuro } from 'react-icons/md'
+import { MdCurrencyPound } from 'react-icons/md'
+import { MdCurrencyFranc } from 'react-icons/md'
 
 const ExchangeRateHeader = () => {
 	return (
 		<section className={styles.wrapper}>
-			<div className={styles.header_img}>
-				<img className={styles.img} src={Money} alt='logo_time' />
-			</div>
+			<BiDollar className={styles.icon_dollar} />
+			<MdOutlineEuro className={styles.icon_euro} />
+			<MdCurrencyPound className={styles.icon_pound} />
+			<MdCurrencyFranc className={styles.icon_franc} />
 			<div className={styles.header_text}>
 				<h1 className={styles.header}>Kurs złotego</h1>
 				<span className={styles.text}>Aktualny kurs złotego względem światowych walut</span>
