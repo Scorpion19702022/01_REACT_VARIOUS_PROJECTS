c51808465b62ef7b1e36bf19d67dd461753061d0
skorpion <jmosinski@o2.pl>
2024-12-07 18:29:09 +0100

    added paragraph and style

--- src/Components/ExchangeRate/ExchangeRateTrendData.tsx
+++ src/Components/ExchangeRate/ExchangeRateTrendData.tsx
@@ -6,6 +6,9 @@ const ExchangeRateTrendData = () => {
 	return (
 		<section className={styles.wrapper}>
 			<h2 className={styles.heading}>Trend</h2>
+			<p className={styles.text_info}>
+				Wykres pokazuje trend kursów walut w przedziale czasowym od 12.11.2024 do 12.12.2024
+			</p>
 		</section>
 	)
 }
