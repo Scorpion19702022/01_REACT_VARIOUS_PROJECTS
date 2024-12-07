6c252a60829e0aac81b83941070bc565fd0448a5
skorpion <jmosinski@o2.pl>
2024-12-07 17:29:48 +0100

    added component for trend data

---
+++ src/Components/ExchangeRate/ExchangeRateTrendData.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Styles/ExchangeRateTrendData.module.css'
+
+const ExchangeRateTrendData = () => {
+	return (
+		<section className={styles.wrapper}>
+			<h2>Trend</h2>
+		</section>
+	)
+}
+
+export default ExchangeRateTrendData
