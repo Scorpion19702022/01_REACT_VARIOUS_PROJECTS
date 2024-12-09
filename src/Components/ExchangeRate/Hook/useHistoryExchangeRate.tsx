9171b18600aa30248f75e72f93423dad112b74b7
skorpion <jmosinski@o2.pl>
2024-12-09 21:15:35 +0100

    added security date

--- src/Components/ExchangeRate/Hook/useHistoryExchangeRate.tsx
+++ src/Components/ExchangeRate/Hook/useHistoryExchangeRate.tsx
@@ -15,7 +15,7 @@ const useHistoryExchageRate = () => {
 		currentDate.setDate(currentDate.getDate() - 2)
 	} else if (dayOfWeek === 0) {
 		currentDate.setDate(currentDate.getDate() - 3)
-	} else if (dayOfWeek === 1 && hour <= 12) {
+	} else if (dayOfWeek === 1) {
 		currentDate.setDate(currentDate.getDate() - 4)
 	} else {
 		currentDate.setDate(currentDate.getDate() - 1)
