9c9abed88f9e5d620027ab37579e7bc3e409c6b7
skorpion <jmosinski@o2.pl>
2024-12-21 16:47:17 +0100

    added hook for chart.js

--- src/Components/ExchangeRate/ExchangeRateChart.tsx
+++ src/Components/ExchangeRate/ExchangeRateChart.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import styles from './Styles/ExchangeRateChart.module.css'
+
+import { ChartData, ChartDataset } from 'chart.js'
 
 import { Line } from 'react-chartjs-2'
 import { Filler } from 'chart.js'
@@ -21,53 +23,62 @@ const ExchangeRateChart = () => {
 
 	ChartJS.register(Filler, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
-	const labels = filteredTrendData.USD.map(item => item.date)
+	const [chartData, setChartData] = useState<ChartData<'line'>>({
+		labels: [],
+		datasets: [],
+	})
 
-	const chartData = {
-		labels,
-		datasets: [
-			{
-				label: 'USD',
-				data: filteredTrendData.USD.map(item => item.value),
-				borderColor: '#228b22',
-				backgroundColor: '#bcdcbc',
-				pointBackgroundColor: '#bcdcbc',
-				fill: false,
-				tension: 0,
-				borderWidth: 2,
-			},
-			{
-				label: 'EUR',
-				data: filteredTrendData.EUR.map(item => item.value),
-				borderColor: '#00ced1',
-				backgroundColor: '#ccf5f5',
-				pointBackgroundColor: '#ccf5f5',
-				fill: false,
-				tension: 0,
-				borderWidth: 2,
-			},
-			{
-				label: 'CHF',
-				data: filteredTrendData.CHF.map(item => item.value),
-				borderColor: '#0048ba',
-				backgroundColor: '#b2c8ea',
-				pointBackgroundColor: '#b2c8ea',
-				fill: false,
-				tension: 0,
-				borderWidth: 2,
-			},
-			{
-				label: 'GBP',
-				data: filteredTrendData.GBP.map(item => item.value),
-				borderColor: '#c32148',
-				backgroundColor: '#edbcc8',
-				pointBackgroundColor: '#edbcc8',
-				fill: false,
-				tension: 0,
-				borderWidth: 2,
-			},
-		],
-	}
+	useEffect(() => {
+		const labels = filteredTrendData.USD.map(item => item.date)
+		console.log('filteredTrendData:', filteredTrendData)
+		setChartData({
+			labels,
+			datasets: [
+				{
+					label: 'USD',
+					data: filteredTrendData.USD.map(item => item.value),
+					borderColor: '#228b22',
+					backgroundColor: '#bcdcbc',
+					pointBackgroundColor: '#bcdcbc',
+					fill: false,
+					tension: 0,
+					borderWidth: 2,
+				},
+				{
+					label: 'EUR',
+					data: filteredTrendData.EUR.map(item => item.value),
+					borderColor: '#00ced1',
+					backgroundColor: '#ccf5f5',
+					pointBackgroundColor: '#ccf5f5',
+					fill: false,
+					tension: 0,
+					borderWidth: 2,
+				},
+				{
+					label: 'CHF',
+					data: filteredTrendData.CHF.map(item => item.value),
+					borderColor: '#0048ba',
+					backgroundColor: '#b2c8ea',
+					pointBackgroundColor: '#b2c8ea',
+					fill: false,
+					tension: 0,
+					borderWidth: 2,
+				},
+				{
+					label: 'GBP',
+					data: filteredTrendData.GBP.map(item => item.value),
+					borderColor: '#c32148',
+					backgroundColor: '#edbcc8',
+					pointBackgroundColor: '#edbcc8',
+					fill: false,
+					tension: 0,
+					borderWidth: 2,
+				},
+			],
+		})
+	}, [filteredTrendData])
+
+	console.log('chartData:', chartData)
 
 	const chartOptions = {
 		responsive: true,
