8f9e3d43ef8f7f6816dadd32340fc23e1f2b47f7
skorpion <jmosinski@o2.pl>
2024-12-26 17:59:09 +0100

    prepared component CalculatorUsing

--- src/Components/Calculator/Calculator.tsx
+++ src/Components/Calculator/Calculator.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import styles from './Styles/Calculator.module.css'
 import CalculatorHeader from './CalculatorHeader'
 import { CalculatorProvider } from './Context/CalculatorContext'
+import CalculatorUsing from './CalculatorUsing'
 
 const Calculator = () => {
 	return (
 		<main className={styles.wrapper}>
 			<CalculatorProvider>
 				<CalculatorHeader />
+				<CalculatorUsing />
 			</CalculatorProvider>
 		</main>
 	)
---
+++ src/Components/Calculator/CalculatorUsing.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Styles/CalculatorUsing.module.css'
+
+const CalculatorUsing = () => {
+	return (
+		<section className={styles.wrapper}>
+			<div>Kalkulator</div>
+		</section>
+	)
+}
+
+export default CalculatorUsing
