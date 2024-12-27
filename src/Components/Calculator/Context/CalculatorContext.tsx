2d83dcae890418e05f57bd51d6c3a1109679105f
skorpion <jmosinski@o2.pl>
2024-12-27 20:28:48 +0100

    return to previous code

--- src/Components/Calculator/Context/CalculatorContext.tsx
+++ src/Components/Calculator/Context/CalculatorContext.tsx
@@ -1,21 +1,12 @@
 import { createContext, useState } from 'react'
 import { Analytics } from '@vercel/analytics/react'
-import { TypeCalculatorFunction, TypeConcatenateFunction } from '../Types/TypesForCalculator'
 
 type InitialStateType = {
 	content: string
-	isEquail?: boolean
-	isLighter?: boolean
-	isMemory?: boolean
-	handleClick: TypeCalculatorFunction | TypeConcatenateFunction
 }
 
 const InitialState: InitialStateType = {
 	content: '',
-	isEquail: true,
-	isLighter: true,
-	isMemory: true,
-	handleClick: 
 }
 
 type CalculatorProviderType = {
@@ -26,14 +17,9 @@ const CalculatorContext = createContext(InitialState)
 
 export const CalculatorProvider = ({ children }: CalculatorProviderType) => {
 	const [content, setContent] = useState<string>('')
-	const [isEquail, setIsEquail] = useState<boolean>(true)
-	const [isLighter, setIsLighter] = useState<boolean>(true)
-	const [isMemory, setIsMemory] = useState<boolean>(true)
-
-	const handleClick = () => {}
 
 	return (
-		<CalculatorContext.Provider value={{ content, isEquail, isLighter, isMemory, handleClick }}>
+		<CalculatorContext.Provider value={{ content }}>
 			{children} <Analytics />
 		</CalculatorContext.Provider>
 	)
