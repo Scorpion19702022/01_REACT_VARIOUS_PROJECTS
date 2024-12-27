14a46f974619794070d37b979ced4ddfa44d13fb
skorpion <jmosinski@o2.pl>
2024-12-27 20:52:57 +0100

    added structure

--- src/Components/Calculator/CalculatorUsing.tsx
+++ src/Components/Calculator/CalculatorUsing.tsx
@@ -6,9 +6,28 @@ const CalculatorUsing = () => {
 	return (
 		<section className={styles.wrapper}>
 			<div className={styles.box_calculator}>
-				<div className={styles.box_buttons}>
-					<button></button>
+				<div className={styles.output}>
+					<div className={styles.previous_operand}></div>
+					<div className={styles.current_operand}></div>
 				</div>
+				<button className={styles.span_two}>AC</button>
+				<button className={styles.span_two}>DEL</button>
+				<button className={styles.span_two}>÷</button>
+				<button className={styles.span_two}>1</button>
+				<button className={styles.span_two}>2</button>
+				<button className={styles.span_two}>3</button>
+				<button className={styles.span_two}>×</button>
+				<button className={styles.span_two}>4</button>
+				<button className={styles.span_two}>5</button>
+				<button className={styles.span_two}>6</button>
+				<button className={styles.span_two}>+</button>
+				<button className={styles.span_two}>7</button>
+				<button className={styles.span_two}>8</button>
+				<button className={styles.span_two}>9</button>
+				<button className={styles.span_two}>˗</button>
+				<button className={styles.span_two}>.</button>
+				<button className={styles.span_two}>0</button>
+				<button className={styles.span_two}>=</button>
 			</div>
 		</section>
 	)
