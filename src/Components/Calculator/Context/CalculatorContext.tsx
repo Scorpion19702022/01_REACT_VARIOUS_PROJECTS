18a2ff2f2eb18bc648fe76d1cfae2c8588f0ee03
skorpion <jmosinski@o2.pl>
2024-12-28 19:12:19 +0100

    added reducer

--- src/Components/Calculator/Context/CalculatorContext.tsx
+++ src/Components/Calculator/Context/CalculatorContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, useReducer, useState } from 'react'
 import { Analytics } from '@vercel/analytics/react'
+import { Action, CalculatorState } from '../Types/TypeForCalculator'
+import { reducer } from '../Reducer'
 
 type InitialStateType = {
 	content: string
@@ -13,13 +15,25 @@ type CalculatorProviderType = {
 	children: React.ReactNode
 }
 
-const CalculatorContext = createContext(InitialState)
+type CalculatorContextType = {
+	content: string
+	dispatch: React.Dispatch<Action>
+}
+
+const initialCalculatorState: CalculatorState = {
+	currentOperand: null,
+	previousOperand: null,
+	operation: null,
+}
+
+const CalculatorContext = createContext<CalculatorContextType | undefined>(undefined)
 
 export const CalculatorProvider = ({ children }: CalculatorProviderType) => {
+	const [state, dispatch] = useReducer(reducer, initialCalculatorState)
 	const [content, setContent] = useState<string>('')
 
 	return (
-		<CalculatorContext.Provider value={{ content }}>
+		<CalculatorContext.Provider value={{ content, dispatch }}>
 			{children} <Analytics />
 		</CalculatorContext.Provider>
 	)
