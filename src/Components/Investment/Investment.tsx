7b7d9175a6fbc7f55c687df2e463c1a2a7d94acd
skorpion <jmosinski@o2.pl>
2025-01-19 16:55:06 +0100

    added component

--- src/Components/Investment/Investment.tsx
+++ src/Components/Investment/Investment.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import styles from './Styles/Investment.module.css'
 import InvestmentHeader from './InvestmentHeader'
+import InvestmentInputs from './InvestmentInputs'
 
 const Investment = () => {
 	return (
 		<main className={styles.wrapper}>
 			<InvestmentHeader />
+			<InvestmentInputs />
 		</main>
 	)
 }
---
+++ src/Components/Investment/InvestmentInputs.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Styles/InvestmentInputs.module.css'
+
+const InvestmentInputs = () => {
+	return (
+		<section className={styles.wrapper}>
+			<div className={styles.box_inputs}></div>
+		</section>
+	)
+}
+
+export default InvestmentInputs
