f57cf910b1788fbe0ddd78dd6e19152c197abc02
skorpion <jmosinski@o2.pl>
2025-01-21 18:55:38 +0100

    added structure for compoent InvestmetResult

--- src/Components/Investment/InvestmentResult.tsx
+++ src/Components/Investment/InvestmentResult.tsx
@@ -3,7 +3,35 @@ import React from 'react'
 import styles from './Styles/InvestmentResult.module.css'
 
 const InvestmentResult = () => {
-	return <section className={styles.wrapper}></section>
+	return (
+		<section className={styles.wrapper}>
+			<div className={styles.box_your_invest}>
+				<h2 className={styles.your_invest}>Zaiwestowałeś: 1000000</h2>
+			</div>
+			<table className={styles.box_name_bank}>
+				<thead className={styles.name_bank}>
+					<tr>
+						<th>Rok</th>
+						<th>PKO BP</th>
+						<th>PKO SA</th>
+						<th>Santander</th>
+						<th>ING</th>
+						<th>mBank</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</section>
+	)
 }
 
 export default InvestmentResult
