fb74b7297a07b119c90373701f8946e134e8809b
skorpion <jmosinski@o2.pl>
2025-01-21 19:05:37 +0100

    added style

--- src/Components/Investment/InvestmentResult.tsx
+++ src/Components/Investment/InvestmentResult.tsx
@@ -6,7 +6,9 @@ const InvestmentResult = () => {
 	return (
 		<section className={styles.wrapper}>
 			<div className={styles.box_your_invest}>
-				<h2 className={styles.your_invest}>Zaiwestowałeś: 1000000</h2>
+				<h2 className={styles.your_invest}>
+					Zaiwestowałeś: <span className={styles.span}>1000000 zł</span>
+				</h2>
 				<button className={styles.btn}>Zobacz wykres</button>
 			</div>
 			<table className={styles.box_name_bank}>
