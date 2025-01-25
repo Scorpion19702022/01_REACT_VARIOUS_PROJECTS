ebc1ad91f21cc527d992d24c0e30f3b9188835ee
skorpion <jmosinski@o2.pl>
2025-01-25 17:50:31 +0100

    added function change

--- src/Components/Investment/Context/InvestmentContext.tsx
+++ src/Components/Investment/Context/InvestmentContext.tsx
@@ -6,6 +6,9 @@ type InitialStateType = {
 	inputInvest: string | number | null
 	inputYearInvest: string | number | null
 	inputTime: string | number | null
+	handleChangeInvest: (e: number) => void
+	handleChangeYearInvest: (e: number) => void
+	handleChangeTime: (e: number) => void
 }
 
 type InvestmentProviderType = {
@@ -16,6 +19,9 @@ const InitialState: InitialStateType = {
 	inputInvest: '',
 	inputYearInvest: '',
 	inputTime: '',
+	handleChangeInvest: (e: number) => {},
+	handleChangeYearInvest: (e: number) => {},
+	handleChangeTime: (e: number) => {},
 }
 
 const InvestmentContext = createContext(InitialState)
@@ -25,8 +31,22 @@ export const InvestmentProvider = ({ children }: InvestmentProviderType) => {
 	const [inputYearInvest, setInputYearInvest] = useState<string | number | null>('')
 	const [inputTime, setInputTime] = useState<string | number | null>('')
 
+	const handleChangeInvest = (e: number) => {
+		setInputInvest(e)
+	}
+
+	const handleChangeYearInvest = (e: number) => {
+		setInputYearInvest(e)
+	}
+
+	const handleChangeTime = (e: number) => {
+		setInputTime(e)
+	}
+
 	return (
-		<InvestmentContext.Provider value={{ inputInvest, inputYearInvest, inputTime }}>
+		<InvestmentContext.Provider
+			value={{ inputInvest, inputYearInvest, inputTime, handleChangeInvest, handleChangeYearInvest, handleChangeTime }}
+		>
 			{children}
 			<Analytics />
 		</InvestmentContext.Provider>
