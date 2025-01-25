03366bf77a82c1780dd382b71d6d65f0a64479ee
skorpion <jmosinski@o2.pl>
2025-01-25 18:44:52 +0100

    added value for inputs

--- src/Components/Investment/InvestmentInputs.tsx
+++ src/Components/Investment/InvestmentInputs.tsx
@@ -25,7 +25,7 @@ const InvestmentInputs = () => {
 					<input
 						className={styles.input}
 						type='number'
-						value={Number(inputInvest || '')}
+						value={Number(inputInvest) || ''}
 						min={0}
 						onChange={e => handleChangeInvest(e.target.value)}
 					/>
@@ -35,7 +35,7 @@ const InvestmentInputs = () => {
 					<input
 						className={styles.input}
 						type='number'
-						value={Number(inputYearInvest || '')}
+						value={Number(inputYearInvest) || ''}
 						min={0}
 						onChange={e => handleChangeYearInvest(e.target.value)}
 					/>
@@ -45,7 +45,7 @@ const InvestmentInputs = () => {
 					<input
 						className={styles.input}
 						type='number'
-						value={Number(inputTime || '')}
+						value={Number(inputTime) || ''}
 						min={1}
 						max={10}
 						onChange={e => handleChangeTime(e.target.value)}
