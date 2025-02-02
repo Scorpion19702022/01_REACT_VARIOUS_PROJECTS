793e5b6df7994d493fe5cd8796363bef025aaae9
skorpion <jmosinski@o2.pl>
2025-02-02 09:53:11 +0100

    added function for buttons

--- src/Components/Investment/InvestmentChart.tsx
+++ src/Components/Investment/InvestmentChart.tsx
@@ -4,11 +4,13 @@ import styles from './Styles/InvestmentChart.module.css'
 import InvestmentContext from './Context/InvestmentContext'
 
 const InvestmentChart = () => {
-	const { chartView } = useContext(InvestmentContext)
+	const { chartView, handleCloseChart } = useContext(InvestmentContext)
 
 	return (
-		<section className={styles.no_wrapper}>
-			<button className={styles.btn_close}>X</button>
+		<section className={!chartView ? styles.no_wrapper : styles.wrapper}>
+			<button className={styles.btn_close} onClick={handleCloseChart}>
+				X
+			</button>
 			<h2 className={styles.heading}>Tu będzie wykres</h2>
 		</section>
 	)
